999e9ade9544e2b5ae1cafc20c5ed230d3090baa
CCDM08 <LABORATORIOMAC@iMac-de-CCDM08.local>
2014-08-15 10:08:47 -0600

    Initial Commit

---
+++ Ejercicio1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ejercicio1
+//
+//  Created by CCDM08 on 15/08/14.
+//  Copyright (c) 2014 anamadrid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
